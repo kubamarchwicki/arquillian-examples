3395f89e5e8c2d9f75b585e9db87a89663fdad7e
kubamarchwicki <kuba@marchwicki.pl>
2013-07-04 21:28:40 +0200

    Removing slf4j dependency

--- application/src/main/java/pl/marchwicki/feedmanager/log/FeedEventLogListener.java
+++ application/src/main/java/pl/marchwicki/feedmanager/log/FeedEventLogListener.java
@@ -1,36 +1,37 @@
 package pl.marchwicki.feedmanager.log;
 
+import java.util.logging.Logger;
+
 import javax.ejb.Asynchronous;
 import javax.ejb.Stateless;
 import javax.enterprise.event.Observes;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 @Stateless
 public class FeedEventLogListener {
 
-	private final static Logger logger = LoggerFactory.getLogger(FeedEventLogListener.class);
-	
+	private final static Logger logger = Logger
+			.getLogger(FeedEventLogListener.class.getName());
+
 	@PersistenceContext
 	EntityManager em;
-	
+
 	@Asynchronous
 	public void processLog(@Observes FeedEventLog log) {
-		logger.info("New feed notification: {} ({} items)", log.getFeedname(), log.getItemsCount());
+		logger.info("New feed notification: " + log.getFeedname() + " " + "("
+				+ log.getItemsCount() + " items)");
 		em.persist(log);
 		em.flush();
 	}
-	
-//	@Asynchronous
-//	public Future<FeedEventLog> processLog(@Observes FeedEventLog log) {
-//		em.persist(log);
-//		em.flush();
-//		em.refresh(log);
-//		
-//		return new AsyncResult<FeedEventLog>(log);
-//	}
-	
+
+	// @Asynchronous
+	// public Future<FeedEventLog> processLog(@Observes FeedEventLog log) {
+	// em.persist(log);
+	// em.flush();
+	// em.refresh(log);
+	//
+	// return new AsyncResult<FeedEventLog>(log);
+	// }
+
 }
